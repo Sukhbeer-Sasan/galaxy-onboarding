7b248a3cf8ce7bfe2ee890c081871b07481598ef
Sukhbeer-Sasan <sukhbeer.sasan@finzly.com>
2026-07-21 19:13:54 +0530

    Fix trade page build issue

--- src/app/(portal)/trade/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ComingSoon } from "@/components/shell/coming-soon";
-import { DOMAINS } from "@/lib/domains";
-
-const d = DOMAINS.find((x) => x.key === "trade")!;
-
-export default function TradePage() {
-  return <ComingSoon title={d.label} blurb={d.blurb} icon={d.icon} />;
-}
